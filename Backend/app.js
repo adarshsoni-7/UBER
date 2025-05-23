68622682decdea6a04ad718b4db11e6a6d8ca300
Adarsh <soniadarsh487@gmail.com>
2025-05-23 23:50:26 +0530

    Set up backend structure with Express, MongoDB connection, and initial routes; added dotenv, cors, and cookie-parser dependencies

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+module.exports = app;
 
